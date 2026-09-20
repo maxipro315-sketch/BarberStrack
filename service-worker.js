2075b9f4a7a42d7d2e6d25487ac01a68adbcf7a0
maxipro315-sketch <maxipro315@gmail.com>
2026-09-19 23:48:01 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'barbertrack-shell-v2';
+const APP_SHELL = [
+  './',
+  './barbertrack_gesti_n_para_barberos.html',
+  './manifest.webmanifest',
+  './icons/barbertrack-192.png',
+  './icons/barbertrack-512.png',
+  './icons/barbertrack-apple.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const networkRequest = fetch(event.request).then(response => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => cached);
+
+      return cached || networkRequest;
+    })
+  );
+});
